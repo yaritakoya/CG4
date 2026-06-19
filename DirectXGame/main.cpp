0a6229e52149627f980a709e78cd3d8b499e4ec3
yaritakoya <k024g0053@g.neec.ac.jp>
2026-06-19 12:33:53 +0900

    修正

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -8,7 +8,7 @@ using namespace KamataEngine;
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	// エンジンの初期化
-	KamataEngine::Initialize(L"CG4_2026_基本プログラム");
+	KamataEngine::Initialize(L"LE3D_25_ヤリタ_コウヤ_CG4");
 
 	// DirectXCommonインスタンスの取得
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
